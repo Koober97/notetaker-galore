41892402e9640ef97e21671e873118c3b60eaff6
Koober97 <koober@Brians-MacBook-Pro.local>
2022-11-08 17:51:58 -0500

    created server routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+app.listen(PORT, () => {
+  console.log(`server live on ${PORT}`);
+});
