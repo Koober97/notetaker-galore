f145adfbe7f90880aa736f2a1dc6f6f4e480a9e9
Koober97 <koober@Brians-MacBook-Pro.local>
2022-11-12 23:08:55 -0500

    messing with server.js

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const express = require('express')
+const { data } = require('./db/db.json')
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
-  console.log(`server live on ${PORT}`);
-});
+    console.log(`Api Server now listening on port 3001`);
+})
